95ec601ebbc4e3028835408ea1896b94ff00be48
李文龙 <liwenlong@himango.cn>
2020-01-15 18:45:38 +0800

    Icache

--- interface.go
+++ interface.go
@@ -1,5 +1,7 @@
 package core
 
+import "time"
+
 type IController interface {
 	SetContext(ctx *Context)
 	GetContext() *Context
@@ -22,4 +24,21 @@ type IError interface {
 type IResponse interface {
 	GetBytes() []byte
 	GetString() string
+}
+
+type ICache interface {
+	GetBytes(key string) []byte
+	GetString(key string) string
+	GetMap(key string) map[string]string
+	GetList(key string) []string
+	GetValue(key string, val *interface{}) error
+
+	SetValue(key string, val interface{}, ex time.Duration) error
+	SetList(key string,  val... interface{}) error
+	SetString(key, val string, ex time.Duration) error
+	SetMap(key string, m map[string]string, ex time.Duration)error
+	SetBytes(key string, b []byte, ex time.Duration) error
+	Expire(key string, ex time.Duration)
+
+	Command(args... string) error
 }
