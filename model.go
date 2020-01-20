74418409bd1abdd282f23b9fe99a75d7c3ef62d2
李文龙 <285753421@qq.com>
2020-01-20 21:56:55 +0800

    format

--- model.go
+++ model.go
@@ -21,57 +21,77 @@ type TableInfo struct {
 	Key   string `json:"key"`
 }
 
+//set model orm with gorm
 func (m *Model) SetGorm(db *gorm.DB) {
 	m.db = db
 }
 
+//alias get model name
 func (m *Model) GetKeyName() string {
 	return m.Info.Table
 }
 
+// get model primary key
 func (m *Model) GetKey() int64 {
-	if m.Value != nil {
-		v, e := m.Value.GetAttrInt64(m.GetKeyName())
-		if e == nil {
-			return v
-		}
+	if !m.checkValueSet() {
+		return 0
 	}
 
-	return 0
+	v, e := m.Value.GetAttrInt64(m.GetKeyName())
+	if e != nil {
+		m.Err = e
+		return 0
+	}
+
+	return v
 }
+
+//alias get model name
 func (m *Model) GetName() string {
 	return m.Info.Table
 }
 
+//get model table name
 func (m *Model) TableName() string {
 	return m.Info.Table
 }
 
+//get real value string
 func (m *Model) GetString() string {
-	if m.Value != nil {
-		b,_ := json.Marshal(m.Value)
-		return string(b[:])
+	if !m.checkValueSet() {
+		return ""
 	}
 
-	return ""
+	b, _ := json.Marshal(m.Value)
+	return string(b[:])
 }
 
+//get real value bytes
 func (m *Model) GetBytes() []byte {
-	if m.Value != nil {
-		b,_ := json.Marshal(m.Value)
-		return b
+	if !m.checkValueSet() {
+		return nil
 	}
 
-	return nil
+	b, _ := json.Marshal(m.Value)
+	return b
 }
 
+//update model m.Value value.primary key
 func (m *Model) Update() bool {
-	if m.Value == nil {
+	if !m.checkValueSet() {
 		return false
 	}
 
-	err := m.db.Model(m.Value).Table(m.TableName()).UpdateColumns(m.Value).Error
+	id, err := m.Value.GetAttrInt64(m.GetKeyName())
 	if err != nil {
+		m.Err = errors.New("must set Value use orm struct")
+		return false
+	}
+
+	err = m.db.Model(m.Value).Table(m.TableName()).
+		Where(m.GetKeyName() + " = " + utils.Int642String(id)).UpdateColumns(m.Value).Error
+	if err != nil {
+		m.Err = err
 		return false
 	}
 
@@ -82,61 +102,86 @@ func (m *Model) Update() bool {
 	return true
 }
 
+//create model with value
 func (m *Model) Create() int64 {
-	o := m.db.New()
-	err := o.Table(m.TableName()).Create(m.Value).Error
-	if err != nil {
+	if !m.checkValueSet() {
 		return 0
 	}
 
-	id,err := m.Value.GetAttrInt64(m.GetKeyName())
+	o := m.db.New()
+	err := o.Table(m.TableName()).Create(m.Value).Error
 	if err != nil {
+		m.Err = err
+		return 0
+	}
+
+	id, err := m.Value.GetAttrInt64(m.GetKeyName())
+	if err != nil {
+		m.Err = err
 		return 0
 	}
 
 	if id < 1 {
+		m.Err = errors.New("insert fail")
 		return 0
 	}
 
 	return id
 }
 
+//delete model with value.primary key
 func (m *Model) Delete() bool {
-	if m.Value != nil {
-		id,err := m.Value.GetAttrInt64(m.GetKeyName())
-		if err != nil {
-			m.Err = errors.New("must set Value use orm struct")
-			return false
-		}
-
-		o := m.db.New()
-		o.Table(m.TableName()).Where(m.GetKeyName()+" = "+utils.Int642String(id)).Delete(m.Value)
-		if o.RowsAffected == 1 {
-			return true
-		}
+	if !m.checkValueSet() {
+		return false
 	}
+
+	id, err := m.Value.GetAttrInt64(m.GetKeyName())
+	if err != nil {
+		m.Err = errors.New("must set Value use orm struct")
+		return false
+	}
+
+	o := m.db.New()
+	o.Table(m.TableName()).Where(m.GetKeyName() + " = " + utils.Int642String(id)).Delete(m.Value)
+	if o.RowsAffected == 1 {
+		return true
+	}
+
+	m.Err = errors.New("rows affected zero")
 
 	return false
 }
 
+//get One Value impl IGetterSetter
 func (m *Model) One(sql string, args ...interface{}) IGetterSetter {
+	if !m.checkValueSet() {
+		return nil
+	}
+
 	typ := reflect.TypeOf(m.Value)
 	value := reflect.New(typ)
 
 	o := m.db.New()
 	err := o.Table(m.TableName()).Where(sql, args).Scan(value.Interface()).Error
 	if err != nil {
+		m.Err = err
 		return nil
 	}
 
-	if v,ok := value.Interface().(IGetterSetter); ok {
+	if v, ok := value.Interface().(IGetterSetter); ok {
 		return v
 	}
 
+	m.Err = errors.New("value not impl IGetterSetter")
 	return nil
 }
 
+//get List Value impl IGetterSetter
 func (m *Model) List(sql string, args ...interface{}) []IGetterSetter {
+	if !m.checkValueSet() {
+		return nil
+	}
+
 	typ := reflect.TypeOf(m.Value)
 
 	modelType := reflect.SliceOf(typ)
@@ -148,6 +193,7 @@ func (m *Model) List(sql string, args ...interface{}) []IGetterSetter {
 	o := m.db.New()
 	err := o.Table(m.TableName()).Where(sql, args).Scan(slice.Interface()).Error
 	if err != nil {
+		m.Err = err
 		return nil
 	}
 
@@ -166,4 +212,14 @@ func (m *Model) List(sql string, args ...interface{}) []IGetterSetter {
 	}
 
 	return nil
+}
+
+//check value is nil
+func (m *Model) checkValueSet() bool {
+	if m.Value == nil {
+		m.Err = errors.New("please set value as impl IGetterSetter")
+		return false
+	}
+
+	return true
 }
