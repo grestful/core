f80313b47abd330cb16bc0c1f3088c69c0924d83
李文龙 <285753421@qq.com>
2020-02-11 23:47:14 +0800

    redis v7

--- init.go
+++ init.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/Unknwon/goconfig"
 	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v7"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/grestful/utils"
 	"github.com/jinzhu/gorm"
