196c79b5a994310066fe6f17fab9195f317162f3
李文龙 <liwenlong@himango.cn>
2020-03-10 16:11:24 +0800

    controller

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ framework core instance
 controller提供了基础类和实现方式
 
 简单控制器实现：
-```go
+```
 func Index(c *gin.Context) {
 	p := &Controller{}
 
@@ -20,7 +20,7 @@ func Index(c *gin.Context) {
 ```
 
 自定义控制器实现:
-```go
+```
 type ExampleService struct {
 	*core.Controller
 }
@@ -36,18 +36,10 @@ func Example(c *gin.Context) {
 }
 ```
 
-# model
-
-本框架内置使用gorm作为model，模型需要支持实现接口：
-```go
-type IModelStruct interface {
-	TableName() string
-	GetKeyName() string
-}
-```
+# model(可用配套mode生成器生成)
 
 示例：
-```go
+```
 type Order struct {
 	Id         uint64  `gorm:"column:id;type:bigint(20) unsigned" json:"id"`
 	OrderId    uint64  `gorm:"column:order_id;type:bigint(20) unsigned" json:"order_id"`
@@ -64,33 +56,12 @@ func (order *Order) GetKeyName() string {
 func (order *Order) TableName() string {
 	return "order"
 }
-
-m := GetModel(&Order{
-    OrderId:1,
-    UserId:2,
-    Name:"ojbk",
-})
-id := m.Create()
-fmt.Println(id)
-ok := m.Update(map[string]interface{"Name":"ok","UserId":1})
-if !ok {
-    fmt.Println(m.Err)
-}
-
-
-list := m.List("UserId = ?", 1)
-for _,v := range list {
-    id, err := v.GetAttrUInt64("id", 0)
-    fmt.Println(id, err)
-}
-
-ok = m.Delete()
-if !ok {
-    fmt.Println(m.Err)
-}
 ```
 
-# session
+# session （1.1.4）
+
+quote from github.com/grest/session
+
 
 # cache
 
--- controller.go
+++ controller.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/grestful/logs"
@@ -15,75 +14,85 @@ import (
 
 type Context struct {
 	*gin.Context
+	Session session.IUserSession
 }
 
 type ProcessFunc func(controller *Controller) IError
 type Controller struct {
+	//log trackId
 	TrackId    string //访问id
-	Session    session.IUserSession
+	//the gin context
 	Ctx        *Context
+	//response code, default success
 	Code       string
+	//controller error
 	error      IError
+	//response data
 	Data       interface{} //output  from data raw data parse
 	Req        interface{} //input params
+
+	// middleware functions
+	Middleware []ProcessFunc
+	// logic functions
 	ProcessFun ProcessFunc
 }
 
-func Session(controller *Controller) error {
-	typ := gGore.GetSessionType()
+//func Session(controller *Controller) error {
+//	typ := gGore.GetSessionType()
+//
+//	sid := ""
+//	cookName, err := gGore.Config.GetValue("", "SESSION_NAME")
+//	if cookName == "" {
+//		cookName = "sid"
+//	}
+//	cook, err := controller.GetContext().Request.Cookie(cookName)
+//	if err != nil {
+//		return err
+//	}
+//	if cook.Value == "" {
+//		sid = controller.GetContext().Request.URL.Query().Get(cookName)
+//	} else {
+//		sid = cook.Value
+//	}
+//
+//	if sid == "" {
+//		return errors.New("sid is empty")
+//	}
+//
+//	maxLeftStr, err := gGore.Config.GetValue("session", "max_left_time")
+//	if err != nil || maxLeftStr == "" {
+//		maxLeftStr = "3600"
+//	}
+//	maxLeft := utils.String2Int64(maxLeftStr, 3600)
+//	switch typ {
+//	case SessionTypeFile:
+//		path, err := gGore.Config.GetValue("session", "file_path")
+//		if err != nil || path == "" {
+//			path = "/tmp"
+//		}
+//
+//		sess := session.GetNewFileSession(path, maxLeft)
+//		controller.Session = session.GetNewUserSession(sid, sess)
+//		return nil
+//	case SessionTypeRedis:
+//		name, err := gGore.Config.GetValue("session", "redis_name")
+//		if err != nil || name == "" {
+//			name = "default"
+//		}
+//		c := gGore.GetRedis(name)
+//		if c == nil {
+//			return errors.New(fmt.Sprintf("can't find redis connection name %s", name))
+//		}
+//		maxLeft := utils.String2Int64(maxLeftStr, 3600)
+//		sess := session.GetNewRedisSession(c, maxLeft)
+//		controller.Session = session.GetNewUserSession(sid, sess)
+//		return nil
+//	}
+//
+//	return errors.New("session type error")
+//}
 
-	sid := ""
-	cookName, err := gGore.Config.GetValue("", "SESSION_NAME")
-	if cookName == "" {
-		cookName = "sid"
-	}
-	cook, err := controller.GetContext().Request.Cookie(cookName)
-	if err != nil {
-		return err
-	}
-	if cook.Value == "" {
-		sid = controller.GetContext().Request.URL.Query().Get(cookName)
-	} else {
-		sid = cook.Value
-	}
-
-	if sid == "" {
-		return errors.New("sid is empty")
-	}
-
-	maxLeftStr, err := gGore.Config.GetValue("session", "max_left_time")
-	if err != nil || maxLeftStr == "" {
-		maxLeftStr = "3600"
-	}
-	maxLeft := utils.String2Int64(maxLeftStr, 3600)
-	switch typ {
-	case SessionTypeFile:
-		path, err := gGore.Config.GetValue("session", "file_path")
-		if err != nil || path == "" {
-			path = "/tmp"
-		}
-
-		sess := session.GetNewFileSession(path, maxLeft)
-		controller.Session = session.GetNewUserSession(sid, sess)
-		return nil
-	case SessionTypeRedis:
-		name, err := gGore.Config.GetValue("session", "redis_name")
-		if err != nil || name == "" {
-			name = "default"
-		}
-		c := gGore.GetRedis(name)
-		if c == nil {
-			return errors.New(fmt.Sprintf("can't find redis connection name %s", name))
-		}
-		maxLeft := utils.String2Int64(maxLeftStr, 3600)
-		sess := session.GetNewRedisSession(c, maxLeft)
-		controller.Session = session.GetNewUserSession(sid, sess)
-		return nil
-	}
-
-	return errors.New("session type error")
-}
-
+//run
 func RunProcess(controller IController, g *gin.Context) {
 	c := GetContext(g)
 	controller.SetContext(c)
@@ -150,6 +159,11 @@ func runProcess(controller IController) (err IError) {
 	return
 }
 
+func (controller *Controller) Use(fn func(controller *Controller) IError) {
+	controller.Middleware = append(controller.Middleware, fn)
+}
+
+// controller default Decode
 func (controller *Controller) Decode() IError {
 	controller.Data = nil
 
@@ -176,13 +190,25 @@ func (controller *Controller) Decode() IError {
 	return nil
 }
 
+// controller default Process
 func (controller *Controller) Process() IError {
+	if controller.Middleware != nil {
+		if len(controller.Middleware) > 0 {
+			for _, m := range controller.Middleware {
+				err := m(controller)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 	if controller.ProcessFun != nil {
 		return controller.ProcessFun(controller)
 	}
 	return nil
 }
 
+//ger ready response
 func (controller *Controller) getResponse() Response {
 	if controller.error != nil {
 		return getDefaultErrorResponse(controller.error)
@@ -191,26 +217,41 @@ func (controller *Controller) getResponse() Response {
 	}
 }
 
+//set error
 func (controller *Controller) SetError(err IError) {
 	GetLog().Info("set error in %s,track_id: %s, err: %s\n", controller.Ctx.Context.Request.URL.Path, controller.TrackId, err.GetMsg())
 	controller.error = err
 	return
 }
 
+//get trackId
 func (controller *Controller) GetTrackId() string {
 	return controller.TrackId
 }
 
+//set trackId
 func (controller *Controller) SetTrackId(id string) {
 	controller.TrackId = id
 }
 
+//set Context
 func (controller *Controller) SetContext(c *Context) {
 	if controller.Ctx == nil {
 		controller.Ctx = c
 	}
 }
 
+//get gin Content
 func (controller *Controller) GetContext() *Context {
 	return controller.Ctx
+}
+
+//query things with gin
+func (controller *Controller) Query(key string) string {
+	return controller.Ctx.Query(key)
+}
+
+//router params with gin
+func (controller *Controller) Param(key string) string {
+	return controller.Ctx.Param(key)
 }
